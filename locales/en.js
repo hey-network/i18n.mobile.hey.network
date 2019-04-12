02ceb5167b9be8442b2230cec5c5cf931b5f188b
Thomas Collart <thomas.collart@get-hey.com>
2019-04-12 15:11:46 +0200

    Init i18n

---
+++ locales/en.js
@@ -0,0 +1,41 @@
+// ************************************************************************** //
+//                                                                            //
+//                                          :::    ::: :::::::::: :::   :::   //
+//   en.js                                 :+:    :+: :+:        :+:   :+:    //
+//                                        +:+    +:+ +:+         +:+ +:+      //
+//   By: tcollart                        +#++:++#++ +#++:++#     +#++:        //
+//                                      +#+    +#+ +#+           +#+          //
+//   Created: 2019/04/12 12:04:50      #+#    #+# #+#           #+#           //
+//   Updated: 2019/04/12 14:44:35     ###    ### ##########    ###.network    //
+//                                                                            //
+//   @format @flow @lint-ignore-every XPLATJSCOPYRIGHT1                       //
+// ************************************************************************** //
+
+export default {
+  AlreadyHaveAnAccount: 'Already have an account?',
+  AreYouSureYouWantToLogOut: 'Are you sure you want to log out?',
+  ByRegisteringYouAcceptOur: 'By registering you accept our',
+  Cancel: 'Cancel',
+  DirectMessages: 'Direct Messages',
+  Discover: 'Discover',
+  edit: 'edit',
+  EditProfile: 'Edit Profile',
+  EmailAddress: 'Email address',
+  FollowedByYourNetwork: 'followed by your network',
+  Followers: 'Followers',
+  Following: 'Following',
+  Logout: 'Logout',
+  No: 'No',
+  NotOnHeyYet: 'Not on Hey yet?',
+  Password: 'Password',
+  ReferralCode: 'Referral code',
+  Save: 'Save',
+  Search: 'Search',
+  Settings: 'Settings',
+  Subscriptions: 'Subscriptions',
+  tagname: 'tagname',
+  TermsOfService: 'Terms of Service',
+  Username: 'Username',
+  WhatDoYouWantToDo: 'What do you want to do?',
+  Yes: 'Yes',
+};
---
+++ locales/fr.js
@@ -0,0 +1,41 @@
+// ************************************************************************** //
+//                                                                            //
+//                                          :::    ::: :::::::::: :::   :::   //
+//   fr.js                                 :+:    :+: :+:        :+:   :+:    //
+//                                        +:+    +:+ +:+         +:+ +:+      //
+//   By: tcollart                        +#++:++#++ +#++:++#     +#++:        //
+//                                      +#+    +#+ +#+           +#+          //
+//   Created: 2019/04/12 12:04:53      #+#    #+# #+#           #+#           //
+//   Updated: 2019/04/12 14:44:34     ###    ### ##########    ###.network    //
+//                                                                            //
+//   @format @flow @lint-ignore-every XPLATJSCOPYRIGHT1                       //
+// ************************************************************************** //
+
+export default {
+  AlreadyHaveAnAccount: 'Deja sur Hey ?',
+  AreYouSureYouWantToLogOut: 'Voulez-vous vraiment vous deconnecter ?',
+  ByRegisteringYouAcceptOur: 'En vous enregistrant vous acceptez',
+  Cancel: 'Annuler',
+  DirectMessages: 'Messages prives',
+  Discover: 'Decouvrir',
+  edit: 'Editer',
+  EditProfile: 'Editer profil',
+  EmailAddress: 'Adresse email',
+  FollowedByYourNetwork: 'Suivi par votre reseau',
+  Followers: 'Followers',
+  Following: 'Following',
+  Logout: 'Se deconnecter',
+  No: 'Non',
+  NotOnHeyYet: 'Pas encore sur Hey ?',
+  Password: 'Mot de passe',
+  ReferralCode: 'Code parrain',
+  Save: 'Sauver',
+  Search: 'Rechercher',
+  Settings: 'Parametres',
+  Subscriptions: 'Abonnements',
+  tagname: 'tagname',
+  TermsOfService: "Conditions d'utilisation",
+  Username: "Nom d'utilisateur",
+  WhatDoYouWantToDo: 'Que voulez-vous faire?',
+  Yes: 'Oui',
+};
